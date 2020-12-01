130316fe564afff4f87f564cf44c4d0c50d6db99
RobinKlincov <44433815+RobinKlincov@users.noreply.github.com>
2020-12-01 23:35:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c;
+            Console.Write("Enter any number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("{0} {1}", a, b);
+
+
+            for (int i = 2; i < number; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
